a96bfe317283d6e31119e328477d96a7c317a413
Vincent Lui <vincent.lui@monash.edu>
2016-06-06 16:10:56 +1000

    Added licensing boilerplate and LICENSE

--- src/QuadTree.h
+++ src/QuadTree.h
@@ -1,3 +1,36 @@
+// This file is part of FivePoint
+// 
+// Copyright (C) 2016 Vincent Lui (vincent.lui@monash.edu), Monash University
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// This file is part of ScaViSLAM.
+//
+// Copyright 2011 Hauke Strasdat (Imperial College London)
+//
+// ScaViSLAM is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// any later version.
+//
+// ScaViSLAM is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with ScaViSLAM.  If not, see <http://www.gnu.org/licenses/>.
+
 #ifndef QUADTREE_H
 #define QUADTREE_H
 
